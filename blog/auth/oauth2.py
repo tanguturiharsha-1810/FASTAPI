535f6acd6031d5365c5ee07e2ff2e9888553485e
Harsha <harshavarshanreddytanguturi@gmail.com>
2026-06-19 12:40:58 +0530

    Fix Swagger OAuth token URL

--- blog/auth/oauth2.py
+++ blog/auth/oauth2.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import blog.auth.tokens as tokens
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
