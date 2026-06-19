07027075751899ee3f553b22d30dd8a27e6843b8
Harsha <harshavarshanreddytanguturi@gmail.com>
2026-06-19 12:26:49 +0530

    added landing message route

--- blog/main.py
+++ blog/main.py
@@ -7,6 +7,18 @@ from blog.router import blog, user, authentication
 
 app= FastAPI()
 
+app= FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI Blog API is Running 🚀"}
+
+Models.Base.metadata.create_all(bind=engine)
+
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
+
 Models.Base.metadata.create_all(bind=engine)
 
 app.include_router(authentication.router)
