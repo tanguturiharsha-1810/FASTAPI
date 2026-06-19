5bd75b82dce6a1e0dae10c4b3b5f694b6e447ad9
Harsha <harshavarshanreddytanguturi@gmail.com>
2026-06-19 13:04:43 +0530

    Fix current user retrieval

--- blog/auth/oauth2.py
+++ blog/auth/oauth2.py
@@ -1,16 +1,22 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import blog.auth.tokens as tokens
+from sqlalchemy.orm import Session
+import blog.database.database as database
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme),db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    return tokens.verify_token(token,credentials_exception)
+    token_data = tokens.verify_token(token, credentials_exception)
+    user = db.query(database.User).filter(database.User.Email == token_data.Email).first()
+    if user is None:
+        raise credentials_exception
+    return user
     
 
    
